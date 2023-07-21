0ebb0442bc1e7b0e729bb95f09b6e09db4b2a562
faroz-khan <fharozkhan143@gmail.com>
2023-07-21 15:29:31 +0530

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div class="spinner-grow" role="status">
+      <span class="sr-only"></span>
+    </div>
+  );
+};
+
+export default Loader;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+
+import App from "./App";
+
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("token");
+  config.headers["x-access-token"] = token;
+
+  return config;
+});
+
+axios.interceptors.response.use(
+  function (response) {return response},
+  function (error) {
+    if (error.response.status === 401) {
+      localStorage.clear();
+      toast.success(
+        "You have been logged out. Login with your credentials to continue!"
+      );
+      window.location.href = "/";
+      window.location.reload();
+    }
+  }
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <ToastContainer />
+    <App />
+  </BrowserRouter>
+);
